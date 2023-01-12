dc0af6355bd2aacfba8983a1729360cd016ae5ad
npdev <sashko.dev@gmail.com>
2023-01-12 18:44:50 +0000

    Server side + two players logic + connection with client

---
+++ server.js
@@ -0,0 +1,28 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+const PORT = 3000;
+
+server.listen(PORT);
+console.log(`Listening on port ${PORT}`);
+
+let readyPlayerCount = 0;
+
+io.on('connection', (socket) => {
+  console.log('User connected as ', socket.id);
+
+  socket.on('ready', () => {
+    console.log('Player ready', socket.id);
+
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      io.emit('startGame', socket.id);
+    }
+  });
+})
